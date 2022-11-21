49c032923dc0c102ed07f0e7805c67e33d28ae00
phsFerreira <pedro.ferreirasilva777@gmail.com>
2022-11-20 21:00:10 -0300

    menu drawer

--- lib/maps.dart
+++ lib/maps.dart
@@ -1,7 +1,6 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'dart:async';
-
+import 'package:connect_bus/headerDrawer.dart';
+import 'package:connect_bus/main.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -145,6 +144,124 @@ class MapSampleState extends State<MapSample> {
         icon: Icon(Icons.menu),
         label: Text('Menu'),
       ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+              child: Column(
+            children: [HeaderDrawer(), DrawerList()],
+          )),
+        ),
+      ),
+    );
+  }
+
+//MENU DRAWER
+  Widget DrawerList() {
+    return Container(
+      padding: EdgeInsets.only(
+        top: 15,
+      ),
+      child: Column(
+        children: [
+          menuItem(),
+        ],
+      ),
+    );
+  }
+
+  Widget menuItem() {
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(
+            Icons.home,
+            size: 25,
+            color: Colors.black,
+          ),
+          title: Text(
+            "Home",
+            style: TextStyle(color: Colors.black, fontSize: 16),
+          ),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(
+            Icons.person,
+            size: 25,
+            color: Colors.black,
+          ),
+          title: Text("Profile"),
+          onTap: () {},
+        ),
+        SizedBox(height: 60),
+
+        //emergencia button
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration:
+                BoxDecoration(color: Colors.white, border: Border.all()),
+            child: Center(
+              child: Text(
+                "emergência",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 20),
+
+        //ajuda button
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration:
+                BoxDecoration(color: Colors.white, border: Border.all()),
+            child: Center(
+              child: Text(
+                "ajuda",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(height: 100),
+
+        //sair button
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: GestureDetector(
+            onTap: () {
+              //volta para a pagina inicial do app
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const MyApp()));
+            },
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
+              child: Center(
+                child: Text(
+                  "sair",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 
